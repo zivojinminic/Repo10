b07aef8359ab619cc2053c969c1c28c96adc845b
zivojinminic <zivojinminic05@gmail.com>
2021-09-22 18:30:45 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Lesi",
+    legs: 4,
+    friends: ["friend1", "friend2"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
